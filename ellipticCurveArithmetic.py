e889df8af26de25fbf7d37110fbc7ad048ea9c4e
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-09-26 10:53:01 +0100

    Mont Curve Addition
    Added Montgomery curve addition, inc. doubling
    
    Removed Elliptic curve class

--- ellipticCurveArithmetic.py
+++ ellipticCurveArithmetic.py
@@ -25,73 +25,56 @@ class Point:
         return Point(self.__x, -self.__y)
 
 
-class EllipticCurve:
-    def __init__(self, a=486662, b=1):
-        self.a = a
-        self.b = b
-
-    def __str__(self):
-        return f'Elliptic curve parameters:\na = {self.a}\nb = {self.b}'
-
-    def addPoints(self, point1, point2):
-        if point1.getPointTuple() == point2.getPointTuple():
-            return self.doublePoint(point1)
-        if point1.isZero():
-            return point2
-        if point2.isZero():
-            return point1
-        if point2.getPointTuple()[0] - point1.getPointTuple()[0] == 0:
-            return Point()
-        L = (point2.getPointTuple()[1] - point1.getPointTuple()[1]) / (point2.getPointTuple()[0] - point1.getPointTuple()[0])
-        x = (L**2) - point1.getPointTuple()[0] - point2.getPointTuple()[0]
-        return Point(x, L * (point1.getPointTuple()[0] - x) - point1.getPointTuple()[1])
-
-    def multiplyPoints(self, point1, scalar):
-        pointCopy = point1.getCopy()
-        r = Point()
-        i = 1
-        while i <= scalar:
-            if i & scalar:
-                r = self.addPoints(r, pointCopy)
-            pointCopy = self.doublePoint(pointCopy)
-            i <<= 1
-        return r
-
-    def doublePoint(self, point):
-        if point.isZero():
-            return point
-        if point.getPointTuple()[1] == 0:
-            return Point()
-        L = 3 * (point.getPointTuple()[0] ** 2) / (2 * point.getPointTuple()[1])
-        x = (L**2) - (2 * point.getPointTuple()[0])
-        return Point(x, L * (point.getPointTuple()[0] - x) - point.getPointTuple()[1])
-
-    def pointFromY(self, y):
-        n = y**2 - self.b
-        if n >= 0:
-            x = n**(1./3)
-        else:
-            x = -((-n)**(1./3))
-        return Point(x, y)
-
-
 class MontgomeryCurve:
-    def __init__(self, A, B, p):
+    def __init__(self, A, B, p, bits):
         # Form: By^2 = x^3 + Ax^2 + x
-        self.A = A
-        self.B = B
-        self.p = p
+        self.A = A # Curve25519 - 486662
+        self.B = B # Curve25519 - 1
+        self.p = p # Curve25519 - (2**255)-19
+        self.bits = bits # Curve25519 - 256
 
     def addPoints(self, point1, point2):
         x1, y1 = point1.getPointTuple()
         x2, y2 = point2.getPointTuple()
 
-        return Point((self.B*((y2-y1)**2))/((x2-x1)**2)-self.A-x1-x2, # x
-                     (((2*x1+x2+self.A)*(y2-y1))/(x2-x1))-((self.B*((y2-y1)**3))/((x2-x1)**3))-y1) # y
+        if x1 == x2 and y1 == y2:
+            return self.doublePoint(point1)
+        elif x1 == x2:
+            'Point at Infinity'
+
+        try:
+            z = (y2 - y1) * inverseMod(x2 - x1, self.p) % self.p
+        except ZeroDivisionError:
+            raise ValueError('Invalid Coordinates.')
+
+        x3 = (self.B * (z**2) - self.A - x1 - x2)%self.p
+
+        return Point(x3, (z * (x1 - x3) - y1)%self.p)
+
+    def doublePoint(self, point):
+        x1, y1 = point.getPointTuple()
+
+        if y1 == 0:
+            return None # todo: amend, point at infinity, find something better than none
+
+        z = (((3 * (x1**2) + 2 * self.A * x1 + 1)%self.p) * inverseMod((2 * self.B * y1)%self.p, self.p))%self.p
+
+        x2 = (self.B * (z**2) - self.A - 2 * x1)%self.p
+
+        return Point(x2, (z * (x1 - x2) - y1)%self.p)
+
+    def scalarMultiple(self, point, scalar):
+        r0 = 0
+        r1 = self.p
+        for i in range(5, -1, -1):
+            pass
+
+def inverseMod(a, p):
+    return pow(a, p-2, p)
 
 
 if __name__ == '__main__':
-    curve = MontgomeryCurve(2, 3, 7)
+    curve = MontgomeryCurve(486662, 1, (2**255)-19, 256)
     pointA = Point(0.04, 0.208)
     pointB = Point(1, 4)
     print('a = ' + str(pointA))
@@ -103,4 +86,3 @@ if __name__ == '__main__':
     #print('c + d = ' + str(curve.addPoints(pointC, pointD)))
     #print('a + b + d = ' + str(curve.addPoints(pointA, curve.addPoints(pointB, pointD))))
     #print('a * 12345 = ', str(curve.multiplyPoints(pointA, 12345)))
-
