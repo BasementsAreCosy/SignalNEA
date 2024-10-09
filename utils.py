521a2b8cc5977c185a3c2b04a2fa68f8df258b4a
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-10-09 09:56:51 +0100

    Extra functions for Curve25519
    Encode Little Endian and Conditional Swap

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
-def decodeLittleEndian(b, bits):
-    return sum([b[i] << 8 * i for i in range((bits + 7) / 8)])
+def encodeLittleEndian(n: int, bits: int):
+    return bytes([(n >> 8 * i) & 0xff for i in range((bits + 7) // 8)])
 
-def decodeScalar25519(k):
-    k_list = [ord(b) for b in k]
+def decodeLittleEndian(b: bytes, bits: int):
+    return sum([b[i] << 8 * i for i in range((bits + 7) // 8)])
+
+def decodeScalar25519(k: bytes):
+    k_list = bytearray(k)
     k_list[0] &= 248
     k_list[31] &= 127
     k_list[31] |= 64
-    return decodeLittleEndian(k_list, 255)
+    return bytes(k_list)
+
+def cswap(swap: int, x2: int, x3: int) -> tuple[int, int]:
+    return (x3, x2) if swap else (x2, x3)
