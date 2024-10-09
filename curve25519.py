e330312093907bbee78b5d40c4bb741077c9b039
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-10-09 09:56:02 +0100

    Completed Curve25519
    Scalar multiplication functioning

--- curve25519.py
+++ curve25519.py
@@ -1,27 +1,24 @@
 from utils import *
 
-# todo: clamp algorithm
-
 class Curve25519:
     def __init__(self):
         self.p = (2**255)-19
-        self.a24 = (486662 - 2) / 4
+        self.a24 = (486662 - 2) // 4
         self.bits = 256
 
-    def decodeUCoordinate(self, u):
-        u_list = [ord(b) for b in u]
+    def decodeUCoordinate(self, u: bytes):
+        u_list = [b for b in u]
         # Ignore any unused bits.
         if self.bits % 8:
             u_list[-1] &= (1 << (self.bits % 8)) - 1
         return decodeLittleEndian(u_list, self.bits)
 
-    def encodeUCoordinate(self, u):
+    def encodeUCoordinate(self, u: int):
         u = u % self.p
-        return ''.join([chr((u >> 8 * i) & 0xff)
-                        for i in range((self.bits + 7) / 8)])
+        return encodeLittleEndian(u, self.bits)
 
-    def scalarMultiplication(self, k, u):
-        _k = decodeScalar25519(k)
+    def scalarMultiplication(self, k: bytes, u: bytes):
+        _k = decodeLittleEndian(k, self.bits)
         _u = self.decodeUCoordinate(u)
 
         x1 = _u
@@ -33,7 +30,11 @@ class Curve25519:
 
         for t in range(self.bits-1, -1, -1):
             kt = (_k >> t) & 1
+
             swap ^= kt
+            x2, x3 = cswap(swap, x2, x3)
+            z2, z3 = cswap(swap, z2, z3)
+            swap = kt
 
             A = (x2 + z2) % self.p
             AA = pow(A, 2, self.p)
@@ -51,4 +52,23 @@ class Curve25519:
 
         x2, x3 = cswap(swap, x2, x3)
         z2, z3 = cswap(swap, z2, z3)
-        return self.encodeUCoordinate(x2 * (z2^(self.p-2)))
+        return self.encodeUCoordinate((x2 * pow(z2, self.p-2, self.p)) % self.p)
+
+if __name__ == '__main__':
+    curve = Curve25519()
+
+    basePoint = curve.encodeUCoordinate(9)
+
+    As = 7
+    Ap = curve.scalarMultiplication(encodeLittleEndian(As, 256), basePoint)
+
+    Bs = 5
+    Bp = curve.scalarMultiplication(encodeLittleEndian(Bs, 256), basePoint)
+
+    ASs = curve.decodeUCoordinate(curve.scalarMultiplication(encodeLittleEndian(As, 256), Bp))
+    BSs = curve.decodeUCoordinate(curve.scalarMultiplication(encodeLittleEndian(Bs, 256), Ap))
+
+    print(ASs)
+    print(BSs)
+
+    print(hex(ASs))
