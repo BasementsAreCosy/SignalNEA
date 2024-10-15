a99b6f751fcf3f31b3d521f04329cff4cb78a202
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-10-15 11:01:27 +0100

    more utils - hashing
    hashing

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
+import hashlib
+
 def encodeLittleEndian(n: int, bits: int):
     return bytes([(n >> 8 * i) & 0xff for i in range((bits + 7) // 8)])
 
 def decodeLittleEndian(b: bytes, bits: int):
     return sum([b[i] << 8 * i for i in range((bits + 7) // 8)])
 
-def decodeScalar25519(k: bytes):
+def clampScalar25519(k: bytes):
     k_list = bytearray(k)
     k_list[0] &= 248
     k_list[31] &= 127
@@ -13,3 +15,12 @@ def decodeScalar25519(k: bytes):
 
 def cswap(swap: int, x2: int, x3: int) -> tuple[int, int]:
     return (x3, x2) if swap else (x2, x3)
+
+def inverseModP(n: int, p: int):
+    return pow(n, p-2, p)
+
+def sha512(x):
+    return hashlib.sha512(x).digest()
+
+def sha512ModQ(x, q):
+    return int.from_bytes(sha512(x), 'little') % q
