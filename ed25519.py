dd99b061977c529a3b9e4604e0a0e38d64bf0139
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-10-23 12:17:13 +0100

    Update ed25519.py

--- ed25519.py
+++ ed25519.py
@@ -1,5 +1,20 @@
 from utils import *
 
+class HomogenousPoint:
+    def __init__(self, X: int, Y: int, Z: int, T: int, p: int):
+        self.X = X
+        self.Y = Y
+        self.Z = Z
+        self.T = T
+        self.p = p
+
+    def negate(self):
+        return HomogenousPoint(-self.X % self.p, self.Y, self.Z, -self.T % self.p)
+
+    def addPoints(self):
+        pass
+
+
 class ed25519:
     def __init__(self):
         self.p = (2 ** 255) - 19
@@ -7,5 +22,5 @@ class ed25519:
         self.q = (2**252) + 27742317777372353535851937790883648493
         self.bits = 256
 
-    def addPoints(self, P, Q):
-        pass # todo: homogenononono point stuff
+    def addPoints(self, P: int, Q: int):
+        pass
