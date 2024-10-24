838142aff0bf06a9653f0796061d3e0b294512ef
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2024-10-24 15:00:34 +0100

    Twisted Edwards add points

--- ed25519.py
+++ ed25519.py
@@ -11,9 +11,6 @@ class HomogenousPoint:
     def negate(self):
         return HomogenousPoint(-self.X % self.p, self.Y, self.Z, -self.T % self.p)
 
-    def addPoints(self):
-        pass
-
 
 class ed25519:
     def __init__(self):
@@ -22,5 +19,19 @@ class ed25519:
         self.q = (2**252) + 27742317777372353535851937790883648493
         self.bits = 256
 
-    def addPoints(self, P: int, Q: int):
-        pass
+    def addPoints(self, point1, point2):
+        A = ((Y1 - X1) * (Y2 - X2)) % p
+        B = ((Y1 + X1) * (Y2 + X2)) % p
+        C = (T1 * 2 * d * T2) % p
+        D = (Z1 * 2 * Z2) % p
+        E = (B - A) % p
+        F = (D - C) % p
+        G = (D + C) % p
+        H = (B + A) % p
+
+        X3 = (E * F) % p
+        Y3 = (G * H) % p
+        T3 = (E * H) % p
+        Z3 = (F * G) % p
+
+        return HomogenousPoint(X3, Y3, T3, Z3, self.p)
